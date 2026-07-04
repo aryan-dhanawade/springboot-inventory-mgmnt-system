d4ca4f04901aa6ca0af92d98b03d79694f9dff18
aryan-dhanawade <aryandhanawade2005@gmail.com>
2026-07-04 12:57:08 +0530

    Intial Project Setup

---
+++ src/test/java/com/aryan/inventory/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
